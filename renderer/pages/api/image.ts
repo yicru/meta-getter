b111326b87e9e2dbaf206cc75f32dafd2309433f
miyamae <yu1ro.mya@gmail.com>
2021-04-13 10:55:15 +0900

    update: api/image アニメーション待機のためにSS撮影前に1秒待つ

--- renderer/pages/api/image.ts
+++ renderer/pages/api/image.ts
@@ -1,20 +1,25 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import playwright from 'playwright'
+import type { NextApiRequest, NextApiResponse } from "next";
+import playwright from "playwright";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
-  const url = req.query.url as string
+  const url = req.query.url as string;
 
-  const browser = await playwright.chromium.launch({ headless: true })
-  const ctx = await browser.newContext()
-  const page = await ctx.newPage()
+  const browser = await playwright.chromium.launch({ headless: true });
+  const ctx = await browser.newContext();
+  const page = await ctx.newPage();
   await page.setViewportSize({
     width: 1200,
     height: 630,
-  })
+  });
 
-  await page.goto(url)
-  const image = await page.screenshot({ type: 'png' })
-  await browser.close()
-  res.setHeader('Content-Type', 'image/png')
-  res.end(image)
-}
+  await page.goto(url);
+  await sleep(1000);
+  const image = await page.screenshot({ type: "png" });
+  await browser.close();
+  res.setHeader("Content-Type", "image/png");
+  res.end(image);
+};
+
+const sleep = async (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
